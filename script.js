5ac1126ff867df7896fa69775892aca5aaf844e5
gryoirsyad <gryoirsyad@apps.ipb.ac.id>
2026-06-15 14:21:13 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateClock(){
+
+    const now = new Date();
+
+    const time = now.toLocaleTimeString();
+
+    document.getElementById("time").innerHTML = time;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();

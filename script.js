06a6964eb71867fb129256d2f4b5d0541d7855f1
gryoirsyad <gryoirsyad@apps.ipb.ac.id>
2026-06-15 14:23:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function updateClock(){
 setInterval(updateClock,1000);
 
 updateClock();
+
+const hour = new Date().getHours();
+
+let greeting = "";
+
+if(hour < 12){
+    greeting = "Good Morning, Sena";
+}
+else if(hour < 18){
+    greeting = "Good Afternoon, Sena";
+}
+else{
+    greeting = "Good Evening, Sena";
+}
+
+document.getElementById("greeting").innerHTML = greeting;
